39914516def7262f25e750b107c7dfb4066263c9
pnd <bp@PND.com>
2017-05-31 21:12:17 -0500

    vr test

--- src/components/aframe-audio-player-component.js
+++ src/components/aframe-audio-player-component.js
@@ -69,6 +69,7 @@ AFRAME.registerComponent('audio-player', {
   onResolvePlayState: function () {
     let { audio } = this.data;
     if (!audio || audio.readyState < 1) {
+      console.log(1);
       return;
     }
 
